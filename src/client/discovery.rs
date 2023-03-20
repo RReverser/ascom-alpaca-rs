cb848165030579dc701bf8361d5ddfa2270a2a05
Ingvar Stepanyan <me@rreverser.com>
2023-03-20 03:57:56 +0000

    Use constant + to_ipv6_mapped

--- src/client/discovery.rs
+++ src/client/discovery.rs
@@ -1,8 +1,7 @@
 use crate::discovery::{
     bind_socket, AlpacaPort, DEFAULT_DISCOVERY_PORT, DISCOVERY_ADDR_V6, DISCOVERY_MSG,
 };
-use net_literals::ipv6;
-use std::net::{IpAddr, SocketAddr};
+use std::net::{IpAddr, Ipv4Addr, SocketAddr};
 use std::time::Duration;
 
 #[derive(Debug, Clone, Copy)]
@@ -54,7 +53,7 @@ impl Client {
                         .await?;
                 }
                 tracing::debug!(request_num, "Sending IPv4 discovery request");
-                let _ = socket.send_to(DISCOVERY_MSG, (ipv6!("::ffff:255.255.255.255"), self.discovery_port)).await?;
+                let _ = socket.send_to(DISCOVERY_MSG, (Ipv4Addr::BROADCAST.to_ipv6_mapped(), self.discovery_port)).await?;
                 let mut buf = [0; 32]; // "{"AlpacaPort":12345}" + some extra bytes for spaces just in case
                 loop {
                     let (len, src) =
