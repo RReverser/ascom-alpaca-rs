87666a1a2bdc0d7ec083ba1a374034e3bbf4298a
Ingvar Stepanyan <me@rreverser.com>
2023-04-02 22:32:58 +0100

    Switch to the extracted serde-ndim

--- src/api/image_array_response.rs
+++ src/api/image_array_response.rs
@@ -1,10 +1,10 @@
 use crate::api::ImageArrayResponseType;
 #[cfg(any(feature = "client", feature = "server"))]
 use crate::client::ResponseTransaction;
-use crate::either::Either;
 use crate::ASCOMResult;
 use bytemuck::{Pod, Zeroable};
-use serde::de::{DeserializeSeed, IgnoredAny, Visitor};
+use ndarray::{Array2, Array3, Axis, Ix3};
+use serde::de::{DeserializeOwned, IgnoredAny, Visitor};
 use serde::{Deserialize, Serialize};
 use serde_repr::{Deserialize_repr, Serialize_repr};
 
@@ -17,12 +17,14 @@ pub enum ImageArrayResponseRank {
 
 #[derive(Debug, PartialEq, Eq, Clone)]
 pub struct ImageArrayResponse {
-    pub data: ndarray::Array3<i32>,
+    pub data: Array3<i32>,
 }
+
+const COLOUR_AXIS: Axis = Axis(2);
 
 impl ImageArrayResponse {
     pub fn rank(&self) -> ImageArrayResponseRank {
-        match self.data.len_of(ndarray::Axis(2)) {
+        match self.data.len_of(COLOUR_AXIS) {
             1 => ImageArrayResponseRank::Rank2,
             _ => ImageArrayResponseRank::Rank3,
         }
@@ -30,29 +32,17 @@ impl ImageArrayResponse {
 
     fn value_as_serialize(&self) -> impl '_ + Serialize {
         #[derive(Serialize)]
-        struct IterSerialize<I: Iterator + Clone>(#[serde(with = "serde_iter::seq")] I)
-        where
-            I::Item: Serialize;
+        #[serde(untagged)]
+        enum Value<'img> {
+            Rank2(#[serde(with = "serde_ndim")] ndarray::ArrayView2<'img, i32>),
+            Rank3(#[serde(with = "serde_ndim")] ndarray::ArrayView3<'img, i32>),
+        }
+
+        let data = self.data.view();
 
         match self.rank() {
-            ImageArrayResponseRank::Rank2 => {
-                Either::Left(IterSerialize(self.data.outer_iter().map(|column| {
-                    column
-                        .to_slice()
-                        .expect("internal arrays should always be in standard layout")
-                })))
-            }
-            ImageArrayResponseRank::Rank3 => Either::Right(IterSerialize(
-                // Slicing is used as a workaround for https://github.com/rust-ndarray/ndarray/issues/1232.
-                (0..self.data.len_of(ndarray::Axis(0))).map(move |column_i| {
-                    IterSerialize((0..self.data.len_of(ndarray::Axis(1))).map(move |row_i| {
-                        self.data
-                            .slice(ndarray::s![column_i, row_i, ..])
-                            .to_slice()
-                            .expect("internal arrays should always be in standard layout")
-                    }))
-                }),
-            )),
+            ImageArrayResponseRank::Rank2 => Value::Rank2(data.remove_axis(COLOUR_AXIS)),
+            ImageArrayResponseRank::Rank3 => Value::Rank3(data),
         }
     }
 }
@@ -126,9 +116,11 @@ impl crate::server::Response for ASCOMResult<ImageBytesResponse> {
             Ok(ImageBytesResponse(ImageArrayResponse { data })) => {
                 metadata.image_element_type = ImageArrayResponseType::Integer as i32;
                 metadata.transmission_element_type = ImageArrayResponseType::Integer as i32;
-                let dims = <[usize; 3]>::try_from(data.shape())
-                    .expect("dimension count mismatch")
-                    .map(|dim| i32::try_from(dim).expect("dimension is too large"));
+                let dims = {
+                    let (dim0, dim1, dim2) = data.dim();
+                    [dim0, dim1, dim2]
+                        .map(|dim| i32::try_from(dim).expect("dimension is too large"))
+                };
                 metadata.dimension_1 = dims[0];
                 metadata.dimension_2 = dims[1];
                 metadata.rank = match dims[2] {
@@ -221,7 +213,7 @@ const _: () = {
                         metadata.transmission_element_type
                     ),
                 };
-                let shape = ndarray::Ix3(
+                let shape = Ix3(
                     usize::try_from(metadata.dimension_1)?,
                     usize::try_from(metadata.dimension_2)?,
                     match metadata.rank {
@@ -275,85 +267,6 @@ impl Serialize for ImageArrayResponse {
     }
 }
 
-struct ValueVisitorCtx {
-    dest: Vec<i32>,
-    first_pass: bool,
-}
-
-impl ValueVisitorCtx {
-    const fn new() -> Self {
-        Self {
-            dest: Vec::new(),
-            first_pass: true,
-        }
-    }
-}
-
-struct ValueVisitor<'data> {
-    ctx: &'data mut ValueVisitorCtx,
-    shape: &'data mut [usize],
-}
-
-impl<'de> Visitor<'de> for ValueVisitor<'_> {
-    type Value = ();
-
-    fn expecting(&self, fmt: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(fmt, "a {}-dimensional array", self.shape.len())
-    }
-
-    fn visit_seq<A: serde::de::SeqAccess<'de>>(self, mut seq: A) -> Result<Self::Value, A::Error> {
-        let (current_shape_len, rest_shape) = self
-            .shape
-            .split_first_mut()
-            .expect("rank should never reach zero");
-        // Store the value of the first pass as the recursion will overwrite it.
-        let first_pass = self.ctx.first_pass;
-        let actual_len = if rest_shape.is_empty() {
-            // Reached the innermost dimension, unmark the first pass.
-            self.ctx.first_pass = false;
-            let len_before = self.ctx.dest.len();
-            while let Some(value) = seq.next_element()? {
-                self.ctx.dest.push(value);
-            }
-            self.ctx.dest.len() - len_before
-        } else {
-            let mut actual_len = 0;
-            while seq
-                .next_element_seed(ValueVisitor {
-                    ctx: self.ctx,
-                    shape: rest_shape,
-                })?
-                .is_some()
-            {
-                actual_len += 1;
-            }
-            actual_len
-        };
-        if first_pass {
-            // If this is the first pass, what we've seen determines the shape for all other passes.
-            *current_shape_len = actual_len;
-        } else if *current_shape_len != actual_len {
-            // Otherwise, we check that the shape is consistent with the first one.
-            return Err(serde::de::Error::invalid_length(
-                actual_len,
-                &current_shape_len.to_string().as_str(),
-            ));
-        }
-        Ok(())
-    }
-}
-
-impl<'de> DeserializeSeed<'de> for ValueVisitor<'_> {
-    type Value = ();
-
-    fn deserialize<D>(self, deserializer: D) -> Result<Self::Value, D::Error>
-    where
-        D: serde::Deserializer<'de>,
-    {
-        deserializer.deserialize_seq(self)
-    }
-}
-
 struct ResponseVisitor;
 
 #[derive(Debug, Deserialize, PartialEq, Eq, Clone, Copy)]
@@ -377,18 +290,19 @@ fn expect_key<'de, A: serde::de::MapAccess<'de>>(
                 continue;
             }
             Some(key) if key == expected_key => Ok(()),
-            Some(key) => Err(serde::de::Error::custom(format!(
-                "expected field {expected_key:?}, got {key:?}"
+            mismatched_key_or_none => Err(serde::de::Error::custom(format!(
+                "expected field {expected_key:?}, got {mismatched_key_or_none:?}"
             ))),
-            None => Err(serde::de::Error::missing_field(match expected_key {
-                KnownKey::Type => "Type",
-                KnownKey::Rank => "Rank",
-                KnownKey::Value => "Value",
-                KnownKey::Other => "(other)",
-            })),
         };
     }
 }
+
+#[derive(Deserialize)]
+#[serde(transparent)]
+struct ResponseData<A>(#[serde(with = "serde_ndim")] A)
+where
+    A: serde_ndim::de::MakeNDim,
+    A::Item: DeserializeOwned;
 
 impl<'de> Visitor<'de> for ResponseVisitor {
     type Value = ImageArrayResponse;
@@ -410,23 +324,18 @@ impl<'de> Visitor<'de> for ResponseVisitor {
         let rank = map.next_value::<ImageArrayResponseRank>()?;
 
         expect_key(&mut map, KnownKey::Value)?;
-        let mut shape = [0, 0, 1];
-        let mut ctx = ValueVisitorCtx::new();
-        map.next_value_seed(ValueVisitor {
-            ctx: &mut ctx,
-            shape: &mut shape[..rank as usize],
-        })?;
+        let data = match rank {
+            ImageArrayResponseRank::Rank2 => map
+                .next_value::<ResponseData<Array2<i32>>>()?
+                .0
+                .insert_axis(COLOUR_AXIS),
+            ImageArrayResponseRank::Rank3 => map.next_value::<ResponseData<Array3<i32>>>()?.0,
+        };
 
         // Consume leftover fields.
-        while map.next_entry::<IgnoredAny, IgnoredAny>()?.is_some() {}
+        let _ = IgnoredAny.visit_map(map)?;
 
-        Ok(ImageArrayResponse {
-            data: ndarray::Array::from_shape_vec(
-                ndarray::Ix3(shape[0], shape[1], shape[2]),
-                ctx.dest,
-            )
-            .expect("couldn't match the parsed shape to the data"),
-        })
+        Ok(ImageArrayResponse { data })
     }
 }
 
