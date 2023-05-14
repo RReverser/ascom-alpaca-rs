9374e4a04873908ecfcb5b40f330343852511569
Ingvar Stepanyan <me@rreverser.com>
2023-05-14 15:29:46 +0100

    Update doc intro

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,5 @@
 /*!
-This is a Rust implementation of the [ASCOM Alpaca API](https://ascom-standards.org/api/).
+This is a Rust implementation of the standard [ASCOM Alpaca API](https://ascom-standards.org/api/) for astronomy devices.
 
 It implements main Alpaca API clients and servers, as well as transparent support for auto-discovery mechanism and `ImageBytes` encoding for camera images.
 
