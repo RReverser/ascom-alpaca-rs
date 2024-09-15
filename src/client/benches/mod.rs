97459061697d6545ac96b230d1d43bf556cbd758
Ingvar Stepanyan <me@rreverser.com>
2024-09-15 02:47:02 +0100

    More benchmarks

--- src/client/benches/mod.rs
+++ src/client/benches/mod.rs
@@ -7,7 +7,7 @@ use criterion::Criterion;
 use mime::APPLICATION_JSON;
 
 macro_rules! declare_parsing_benches {
-    ($($name:ident: $ty:ty => $fixture_path:literal,)*) => {
+    ($($name:ident: $ty:ty => ($mime:expr, $fixture_path:literal),)*) => {
         /// Run response parsing benchmarks against stored fixtures.
         pub fn benches() {
             let _ =
@@ -17,7 +17,7 @@ macro_rules! declare_parsing_benches {
                     .bench_function(stringify!($name), |b| {
                         b.iter(move || {
                             <$ty>::from_reqwest(
-                                APPLICATION_JSON,
+                                $mime,
                                 Bytes::from_static(include_bytes!($fixture_path)),
                             )
                             .expect("Failed to parse fixture")
@@ -30,7 +30,9 @@ macro_rules! declare_parsing_benches {
 
 declare_parsing_benches! {
     parse_configured_devices: ValueResponse<Vec<ConfiguredDevice<FallibleDeviceType>>>
-        => "resp_configured_devices.json",
+        => (APPLICATION_JSON, "resp_configured_devices.json"),
     parse_image_array: ASCOMResult<ImageArray>
-        => "resp_image_array.json",
+        => (APPLICATION_JSON, "resp_image_array.json"),
+    parse_image_array_bin: ASCOMResult<ImageArray>
+        => ("application/imagebytes".parse().expect("couldn't parse mime type"), "resp_image_array.bin"),
 }
