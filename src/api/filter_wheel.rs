0546defb27cf594b2618d162f766ae9ce2e4050a
Ingvar Stepanyan <me@rreverser.com>
2026-01-13 13:32:57 +0000

    Fix FilterWheel.position
    The getter returns an additional "moving" state indicated by `-1` in the ASCOM specification.
    
    We map it as `Option::None` instead.

--- src/api/filter_wheel.rs
+++ src/api/filter_wheel.rs
@@ -14,8 +14,10 @@ pub trait FilterWheel: Device + Send + Sync {
     async fn names(&self) -> ASCOMResult<Vec<String>>;
 
     /// Returns the current filter wheel position.
-    #[http("position", method = Get, device_state = "Position")]
-    async fn position(&self) -> ASCOMResult<usize>;
+    ///
+    /// Note: `None` indicates that the filter wheel is currently moving (equivalent to `-1` in the ASCOM specification).
+    #[http("position", method = Get, via = OptionalPosition, device_state = "Position")]
+    async fn position(&self) -> ASCOMResult<Option<usize>>;
 
     /// Sets the filter wheel position.
     #[http("position", method = Put)]
@@ -29,5 +31,30 @@ pub trait FilterWheel: Device + Send + Sync {
     #[http("interfaceversion", method = Get)]
     async fn interface_version(&self) -> ASCOMResult<u16> {
         Ok(3)
+    }
+}
+
+#[derive(derive_more::From, derive_more::Into)]
+pub(super) struct OptionalPosition(Option<usize>);
+
+impl serde::Serialize for OptionalPosition {
+    fn serialize<S: serde::Serializer>(&self, serializer: S) -> Result<S::Ok, S::Error> {
+        self.0.map_or(-1, usize::cast_signed).serialize(serializer)
+    }
+}
+
+impl<'de> serde::Deserialize<'de> for OptionalPosition {
+    fn deserialize<D: serde::Deserializer<'de>>(deserializer: D) -> Result<Self, D::Error> {
+        let value = i64::deserialize(deserializer)?;
+
+        Ok(Self(match value {
+            -1 => None,
+            _ => Some(value.try_into().map_err(|_err| {
+                serde::de::Error::invalid_value(
+                    serde::de::Unexpected::Signed(value),
+                    &"-1 (moving) or or a non-negative filter wheel position",
+                )
+            })?),
+        }))
     }
 }
